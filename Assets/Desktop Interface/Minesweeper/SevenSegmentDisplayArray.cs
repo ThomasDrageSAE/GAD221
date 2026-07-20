377158a431f5b0803d6f4e0630f9cd2a4f4a2c73
FrankManfordSAE <1032183@student.sae.edu.au>
2026-07-20 13:29:18 +0800

    Seven Segment Display Array Changes
    - Changed behaviour to display on the right side of the display.
    - Changed behaviour to allow displaying negative numbers
    - Fixed a couple bugs & removed redundant methods.

--- Assets/Desktop Interface/Minesweeper/SevenSegmentDisplayArray.cs
+++ Assets/Desktop Interface/Minesweeper/SevenSegmentDisplayArray.cs
@@ -8,12 +8,11 @@ public class SevenSegmentDisplayArray : MonoBehaviour
     private int arrayValue;
     private int displayCount;
     private int maximumValue;
+    private int minimumValue;
 
     private void Start()
     {
         Initialize();
-        // Testing Remove
-        SetValue(256);
     }
 
     public void Initialize()
@@ -27,48 +26,67 @@ public class SevenSegmentDisplayArray : MonoBehaviour
         {
             maxValueString += "9";
         }
-
-        try
-        {
-            maximumValue = Convert.ToInt32(maxValueString);
-        }
         
-        catch (Exception e)
-        {
-            Debug.Log(e);
-        }
+        maximumValue = Convert.ToInt32(maxValueString);
+        minimumValue = -(maximumValue / 10);
+        
+        Debug.Log("Maximum Value: " + maximumValue);
+        Debug.Log("Minimum Value: " + minimumValue);
     }
 
     public void SetValue(int value)
     {
         //Debug.Log("SevenSegmentDisplayArray - SetValue: " + value);
-        
-        arrayValue = value;
-        SetChildValues();
-    }
 
-    private void SetChildValues()
-    {
-        //Debug.Log("SevenSegmentDisplayArray - SetChildValues: ");
-        
-        string valueString = arrayValue.ToString();
-        
-        for (int i = 0; i < displayCount; i++)
+        if (value <= maximumValue && value >= minimumValue)
         {
-            if (i < valueString.Length - 1)
-            {
-                displays[i].SetValue(Convert.ToInt32(valueString.Substring(i, 1)));
-            }
-
-            else
-            {
-                displays[i].SetValue(0);
-            }
+            arrayValue = value;
+            SetChildValues(arrayValue);
+        }
+        
+        else
+        {
+            Debug.Log("Value is outside of range for amount of displays");
         }
     }
 
-    public int GetValue()
+    private void SetChildValues(int value)
     {
-        return arrayValue;
+        string valueString = value.ToString();
+        int valueLength = valueString.Length;
+        
+        if (valueLength > displays.Count)
+        {
+            Debug.Log("Value length is greater than the amount of displays");
+            return;
+        }
+        
+        int emptyDisplays = displayCount - valueLength;
+        
+        for (int i = 0; i < displayCount; i++)
+        {
+            SevenSegmentDisplay display = displays[i];
+            char character = valueString[i];
+            
+            if (i < emptyDisplays)
+            {
+                display.SetValue(-1);
+                continue;
+            }
+
+            if (character.Equals('-'))
+            {
+                display.SetValue(10);
+                continue;
+            }
+
+            if (character >= '0' && character <= '9')
+            {
+                display.SetValue(Convert.ToInt32(character));
+                continue;
+            }
+            
+            Debug.Log("Invalid Display Character: " + character);
+        }
     }
 }
